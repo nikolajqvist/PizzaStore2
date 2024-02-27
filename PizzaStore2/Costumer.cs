411fdb65e1902c5f49c8918052c1709085c05759
Nikolaj Qvist Møller <nqvistmoller@hotmail.com>
2024-02-27 14:07:25 +0100

    alt slettet

--- PizzaStore2/Costumer.cs
+++ PizzaStore2/Costumer.cs
@@ -6,24 +6,8 @@ using System.Threading.Tasks;
 
 namespace PizzaStore2
 {
-    internal class Costumer
+    public class Costumer
     {
-        private string _name;
-        private int _age;
-        private string _adress;
 
-        public string Name { get => _name; set => _name = value; }
-        public int Age { get => _age; set => _age = value; }
-        public string Adress { get => _adress; private set => _adress = value; }
-        public Costumer(string name, int age)
-        {
-            _adress = Adress;
-            _name = name;
-            _age = age;
-        }
-        public override string ToString()
-        {
-            return $"{Name} har bestilt en ";
-        }
     }
 }
--- PizzaStore2/Order.cs
+++ PizzaStore2/Order.cs
@@ -6,33 +6,8 @@ using System.Threading.Tasks;
 
 namespace PizzaStore2
 {
-    internal class Order
+    public class Order
     {
-        private int _orderNumber;
-        private int _id;
-        private static int idcounter = 0;
-        private DateTime _date;
-        private Pizza _pizza;
 
-        public int OrderNumber { get => _orderNumber; private set => _orderNumber = value; }
-        public int Id { get => _id; set => _id = value; }
-        public DateTime Date { get => _date; set => _date = value; }
-        internal Pizza Pizza { get => _pizza; set => _pizza = value; }
-
-        public Order(string pizzaname, int price)
-        {
-            Id = idcounter;
-            idcounter++;
-            Date = DateTime.Now;
-            _pizza = new Pizza("", 0);
-        }
-        public override string ToString()
-        {
-            return "Ordrenr:" + Id + " er kommet " + Date.ToString("kl: HH:mm: ss");
-        }
-        public void Start()
-        {
-
-        }
     }
 }
